02fd69c5525036a191e79f6a3f04fd2133e490b2
JoseCarlosRH <josecarlosrh03@gmail.com>
2020-03-14 21:26:45 -0400

    creacion de la tabla LostPassword

---
+++ Models/PassworLost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WaterSolutionAPI.Models
+{
+    public partial class PassworLost
+    {
+        public int IdPassworLost { get; set; }
+        public string Extencion { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public virtual Usuarios IdUsuarioNavigation { get; set; }
+    }
+}
--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -5,12 +5,18 @@ namespace WaterSolutionAPI.Models
 {
     public partial class Usuarios
     {
+        public Usuarios()
+        {
+            PassworLost = new HashSet<PassworLost>();
+        }
+
         public int IdUsuario { get; set; }
         public int EmpleadoUsuarioidEmpleado { get; set; }
         public string NombreUsuario { get; set; }
-        public byte PasswordUsuario { get; set; }
+        public string PasswordUsuario { get; set; }
         public bool EstadoUsuario { get; set; }
 
         public virtual Empleados EmpleadoUsuarioidEmpleadoNavigation { get; set; }
+        public virtual ICollection<PassworLost> PassworLost { get; set; }
     }
 }
