6726a133babcb8abc2062730842cbfbca45c25bb
ramon05 <ramoncastillo051991@gmail.com>
2020-03-19 22:13:07 -0400

    Mantenimiento de DetalleCotizacion

---
+++ Interfaces/IDetalleCotizacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterSolutionAPI.Models;
+
+namespace WaterSolutionAPI.Interfaces
+{
+	public interface IDetalleCotizacion
+	{
+		Task<DetalleCotizacion> save(DetalleCotizacion model);
+		Task<DetalleCotizacion> Edit(DetalleCotizacion model);
+		bool Exists(int id);
+	}
+}
---
+++ Servicios/DetalleCotizacionService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterSolutionAPI.Models;
+using WaterSolutionAPI.Properties;
+using WaterSolutionAPI.WaterSolutionDBC;
+
+namespace WaterSolutionAPI.Servicios
+{
+	public class DetalleCotizacionService : IDetalleCotizacion
+	{
+		private readonly WaterSolutionDBContext _context;
+
+		public async Task<DetalleCotizacion> save(DetalleCotizacion model)
+		{
+			await _context.DetalleCotizacion.AddAsync(model);
+			await _context.SaveChangesAsync();
+			return model;
+		}
+
+		public async Task<DetalleCotizacion> Edit(DetalleCotizacion model)
+		{
+			try
+			{
+				_context.Entry(model).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			return model;
+		}
+
+		public bool Exists(int id)
+		{
+			return _context.DetalleCotizacion.Any(x => x.DetalleCotizacionId == id);
+		}
+	}
+}
