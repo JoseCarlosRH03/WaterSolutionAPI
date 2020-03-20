96c8b5838e65284bd12f6c7a0161a6bded7e6638
ramon05 <ramoncastillo051991@gmail.com>
2020-03-19 20:07:13 -0400

    Mantenimiento de Departamentos

---
+++ Interfaces/IDepartamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterSolutionAPI.Models;
+
+namespace WaterSolutionAPI.Interfaces
+{
+	public interface IDepartamento
+	{
+		Task<Departamentos> save(Departamentos model);
+		Task<Departamentos> Edit(Departamentos model);
+		bool Exists(int id);
+	}
+}
---
+++ Servicios/DepartamentoServices.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterSolutionAPI.Interfaces;
+using WaterSolutionAPI.Models;
+using WaterSolutionAPI.WaterSolutionDBC;
+
+namespace WaterSolutionAPI.Servicios
+{
+	public class DepartamentoServices : IDepartamento
+	{
+		private readonly WaterSolutionDBContext _context;
+
+		public DepartamentoServices(WaterSolutionDBContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Departamentos> save(Departamentos model)
+		{
+			await _context.Departamentos.AddAsync(model);
+			await _context.SaveChangesAsync();
+			return model;
+		}
+
+		public async Task<Departamentos> Edit(Departamentos model)
+		{
+			try
+			{
+				_context.Entry(model).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
+			return model;
+		}
+
+		public bool Exists(int id)
+		{
+			return _context.Departamentos.Any(x => x.IdDepartamento == id);
+		}
+	}
+}
