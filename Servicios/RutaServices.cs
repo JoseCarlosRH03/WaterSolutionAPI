8cdeabfecad55f10af18f9d4fc3dfb478ace90b6
ramon05 <ramoncastillo051991@gmail.com>
2020-03-21 23:18:06 -0400

    Mantenimiento de Ruta

---
+++ Servicios/RutaServices.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterSolutionAPI.Interfaces;
+using WaterSolutionAPI.Models;
+using WaterSolutionAPI.WaterSolutionDBC;
+
+namespace WaterSolutionAPI.Servicios
+{
+	public class RutaServices : IRuta
+	{
+		private readonly WaterSolutionDBContext _context;
+
+		public RutaServices(WaterSolutionDBContext context)
+		{
+			_context = context;
+		}
+		public async Task<Ruta> save(Ruta model)
+		{
+			await _context.Ruta.AddAsync(model);
+			await _context.SaveChangesAsync();
+
+			return model;
+		}
+
+		public async Task<Ruta> Edit(Ruta model)
+		{
+			try
+			{
+				_context.Entry(model).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			return model;
+		}
+
+		public bool Exists(int id) => _context.Ruta.Any(x => x.RutaId == id);
+	}
+}
